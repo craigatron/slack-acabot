07db2094084123b6f6cc668cded938071ff8f616
Craig Martek <craigmartek@gmail.com>
2018-05-21 19:26:21 -0700

    finishing the initial bot

--- acabot.py
+++ acabot.py
@@ -12,31 +12,41 @@ from httplib2 import Http
 from slackclient import SlackClient
 import datetime
 
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets',
-        'https://www.googleapis.com/auth/drive']
+SCOPES = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive'
+]
 
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', SCOPES)
+creds = ServiceAccountCredentials.from_json_keyfile_name(
+    'client_secret.json', SCOPES)
 
 client = gspread.authorize(creds)
 
-SPREADSHEET = '1Xkkjg7WSrZE7nHRT5OxYl4hpamvJa8D3GvBMLo4wceI'
+SPREADSHEET = os.environ.get('SPREADSHEET_ID')
+CHANNEL_ID = os.environ.get('CHANNEL_ID')
 
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
-slack_client = SlackClient(os.environ.get('SLACK_OAUTH_TOKEN'))
+SLACK_CLIENT = SlackClient(os.environ.get('SLACK_OAUTH_TOKEN'))
+
 
 def is_valid(request):
-    return request.get_json()['token'] == os.environ.get('SLACK_VERIFICATION_TOKEN')
+    return request.form['token'] == os.environ.get('SLACK_VERIFICATION_TOKEN')
+
 
 @app.route('/attendance', methods=['POST'])
 def attendance():
-    logging.warning('attendance request: %s', request.get_json())
+    logging.info('attendance request: %s', request.form)
     if not is_valid(request):
-        print('invalid token ' + request.get_json()['token'])
+        print('invalid token ' + request.form['token'])
         abort(400)
 
-    pieces = request.get_json()['text'].split()
+    sheet = client.open_by_key(SPREADSHEET).sheet1
+    if not request.form['text'].strip():
+        return _get_user_attendances(request.form['user_name'], sheet)
+
+    pieces = request.form['text'].split()
 
     if pieces[0] == 'help':
         return _get_help_text()
@@ -46,9 +56,12 @@ def attendance():
     except ValueError:
         return jsonify(text='%s doesn\'t look like a date' % pieces[0])
 
-    sheet = client.open_by_key(SPREADSHEET).sheet1
-    date_cell = sheet.find(pieces[0])
-    if not date_cell:
+    try:
+        date_cell = sheet.find(pieces[0])
+        if not date_cell:
+            return jsonify(
+                text='%s not found in the spreadsheet :(' % pieces[0])
+    except gspread.exceptions.CellNotFound:
         return jsonify(text='%s not found in the spreadsheet :(' % pieces[0])
 
     if len(pieces) == 1:
@@ -56,22 +69,88 @@ def attendance():
 
     return _record_attendance(date_cell, sheet, pieces)
 
+
+def _get_user_attendances(username, sheet):
+    user_cells = sheet.findall(re.compile('^' + username))
+    if not user_cells:
+        return jsonify(text='No upcoming attendance records found.')
+
+    lines = []
+    for cell in user_cells:
+        rowdatestring = sheet.cell(cell.row, 1).value
+        rowdate = datetime.datetime.strptime(rowdatestring, '%Y-%m-%d').date()
+        if rowdate < datetime.datetime.today().date():
+            continue
+
+        pieces = cell.value.split(' | ')
+        if len(pieces) > 2 and ' '.join(pieces[2:]):
+            text = '*%s:* %s "%s"' % (rowdatestring, pieces[1],
+                                      ' '.join(pieces[2:]))
+        else:
+            text = '*%s:* %s' % (rowdatestring, pieces[1])
+
+        lines.append(text)
+
+    if not lines:
+        return jsonify(text='No upcoming attendance records found.')
+
+    return jsonify(text='\n'.join(lines))
+
+
 def _get_attendance(date_cell, sheet):
-    row_values = sheet.row_values(date_cell.row)
-    return jsonify(text=' '.join(row_values))
+    yes = []
+    no = []
+    maybe = []
+    users_found = []
+    for cell in sheet.range(date_cell.row, 2, date_cell.row, sheet.col_count):
+        if cell.value:
+            pieces = cell.value.split(' | ')
+            users_found.append(pieces[0])
+            if pieces[1] == 'yes':
+                yes.append(pieces[0])
+            elif pieces[1] == 'maybe':
+                maybe.append((pieces[0], ' '.join(pieces[2:])))
+            elif pieces[1] == 'no':
+                no.append((pieces[0], ' '.join(pieces[2:])))
+
+    no_response = [u for u in _get_active_users() if u not in users_found]
+    all_yeses = yes + ['%s (no response)' % u for u in no_response]
+
+    text = '\n'.join([
+        '*Yes:*', ', '.join(all_yeses) or '_no entries found_', '', '*Maybe:*',
+        ', '.join(['%s "%s"' % (u[0], u[1])
+                   for u in maybe]) or '_no entries found_', '', '*No:*',
+        ', '.join(['%s "%s"' % (u[0], u[1])
+                   for u in no]) or '_no entries found_'
+    ])
+
+    return jsonify(text=text)
+
+
+def _get_active_users():
+    channel_info = SLACK_CLIENT.api_call('channels.info', channel=CHANNEL_ID)
+    active_members = channel_info['channel']['members']
+    all_members = SLACK_CLIENT.api_call('users.list')
+    return [
+        u['profile']['display_name'] for u in all_members['members']
+        if u['id'] in active_members
+    ]
 
 
 def _record_attendance(date_cell, sheet, pieces):
     if pieces[1] not in ['yes', 'no', 'maybe']:
-        return jsonify(text='2nd argument must be one of "yes", "no", or "maybe"')
+        return jsonify(
+            text='2nd argument must be one of "yes", "no", or "maybe"')
 
-    if len(pieces) < 3:
+    if pieces[1] != 'yes' and len(pieces) < 3:
         return jsonify(text='Reason is required')
 
-    sheet.update_cell(
-            date_cell.row,
-            _get_user_column(sheet, date_cell.row, request.get_json()['user_name']),
-            ' | '.join([request.get_json()['user_name'], pieces[1], ' '.join(pieces[2:])]))
+    sheet.update_cell(date_cell.row,
+                      _get_user_column(sheet, date_cell.row,
+                                       request.form['user_name']), ' | '.join([
+                                           request.form['user_name'],
+                                           pieces[1], ' '.join(pieces[2:])
+                                       ]))
     return jsonify(text='Recorded!')
 
 
@@ -91,4 +170,14 @@ def _get_user_column(sheet, row, username):
 
 
 def _get_help_text():
-    return jsonify(text='here\'s some help')
+    text = """
+Request all of your upcoming attendances: */attendance*
+
+See attendance for a particular date: */attendance {date}*
+    e.g. */attendance 2018-08-01*
+
+Record your attendance: */attendance {date} {yes|no|maybe} {reason}*
+    e.g. */attendance 2018-08-01 no on vacation!*
+    (reason is only required for "no" and "maybe")
+    """
+    return jsonify(text=text)
